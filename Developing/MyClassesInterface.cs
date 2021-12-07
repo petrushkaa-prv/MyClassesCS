1c6505044c69ccbd584d375e14aa7dc5ac9400ad
kjaka <kjaka@ADAM-PC>
2021-12-07 13:38:45 +0100

    [time]:[21:55] [date]:[7.12.21] Add project files.

---
+++ Developing/MyClassesInterface.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Developing.MyClasses
+{
+    /// <summary>
+    /// Interface of general methods to be present in all MyClasses Classes
+    /// </summary>
+    interface IMyClasses<T>
+    {
+        void FillWith(T element, int howMany = 0);
+    }
+}
