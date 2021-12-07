3f40f513b4c429ec6d2c9cdb4c194506de962dd9
98Des <kjakadam@gmail.com>
2021-12-07 23:37:30 +0100

    [23:37] [7.12.21]

--- Program.cs
+++ Program.cs
@@ -7,7 +7,8 @@ namespace Developing
 {
     public static class StackExtender
     {
-        public static IEnumerable<N> DoWork<T, N>(this MyClasses.List.Stack<T> stack, Func<T, N> func)
+        public static IEnumerable<N> DoWork<T, N>(this MyClasses.List.Stack<T> stack, Func<T, N> func) 
+            where T : IComparable<T>
         {
             foreach (var VARIABLE in stack)
             {
@@ -15,7 +16,8 @@ namespace Developing
             }
         }
 
-        public static MyClasses.List.Stack<T> DoWork2<T>(this MyClasses.List.Stack<T> stack, Func<(T, T), T> func)
+        public static MyClasses.List.Stack<T> DoWork2<T>(this MyClasses.List.Stack<T> stack, Func<(T, T), T> func) 
+            where T : IComparable<T>
         {
             var res = (T[])stack;
 
@@ -27,7 +29,9 @@ namespace Developing
             return res;
         }
 
-        public static MyClasses.List.Stack<N> Convert<T, N>(this MyClasses.List.Stack<T> stack)
+        public static MyClasses.List.Stack<N> Convert<T, N>(this MyClasses.List.Stack<T> stack) 
+            where N : IComparable<N> 
+            where T : IComparable<T>
         {
             MyClasses.List.Stack<N> res = new MyClasses.List.Stack<N>();
 
@@ -38,14 +42,30 @@ namespace Developing
 
             return res;
         }
+
+        public static MyClasses.List.Stack<N> Convert<T, N>(this MyClasses.List.Stack<T> stack, Func<T,N> func)
+            where N : IComparable<N>
+            where T : IComparable<T>
+        {
+            MyClasses.List.Stack<N> res = new MyClasses.List.Stack<N>();
+
+            foreach (var VARIABLE in stack.Reverse())
+            {
+                res.Push(func(VARIABLE));
+            }
+
+            return res;
+        }
     }
 
     static class Program
     {
         static void Main(string[] args)
         {
+            MyClasses.List.Stack<int> intchars =
+                new MyClasses.List.Stack<char>("Something".ToCharArray()).Convert(lambda => (int)lambda - 48);
 
-
+            Console.WriteLine(intchars);
         }
     }
 }
